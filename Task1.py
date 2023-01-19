93ce375c50a9b3e2ef54f86201554cfaedc70440
esterkin_aleksandr <esterkin.alx@gmail.com>
2023-01-20 00:04:58 +0300

    Task1new

--- Task1.py
+++ Task1.py
@@ -4,9 +4,11 @@ a = x + y
 b = x - y
 c = x * y
 d = x / y
+e = x // y
 w1 = 0
 w2 = 0
 w3 = 0
+w4 = 0
 if a > b:
     w1 = a
 else:
@@ -19,4 +21,8 @@ if w2 > d:
     w3 = w2
 else:
     w3 = d
-print (w3)
+if w3 > e:
+    w4 = w3
+else:
+    w4 = e
+print (w4)
