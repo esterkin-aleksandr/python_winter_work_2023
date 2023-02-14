4a815dad73a9c2fa5009c1bd11d9b2b3d8259e9f
esterkin_aleksandr <esterkin.alx@gmail.com>
2023-02-14 14:20:47 +0300

    Task_10.1

--- Task_10.1.py
+++ Task_10.1.py
@@ -1,6 +1,5 @@
 import openpyxl
 from openpyxl import Workbook
-wb = Workbook()
 # wb.save('Task_10.1_ex.xlsx')
 wb = openpyxl.load_workbook('Task_10.1_ex.xlsx')
 # wb.create_sheet("Итог")
@@ -8,7 +7,7 @@ wb = openpyxl.load_workbook('Task_10.1_ex.xlsx')
 ws1=wb['Sheet']
 ws2=wb['Итог']
 sotrsum = {}
-for i in range(1,ws1.max_row):
+for i in range(1,ws1.max_row+1):
     sotr = ws1.cell(row=i, column=1).value
     sotrsum[sotr] = sotrsum.get(sotr, 0) + ws1.cell(row=i, column=2).value
 print(sotrsum)
