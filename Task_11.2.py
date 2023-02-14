e51359da8c68556407254fbfb931e54475db56a7
esterkin_aleksandr <esterkin.alx@gmail.com>
2023-02-14 15:37:52 +0300

    Task_11.2

--- Task_11.2.py
+++ Task_11.2.py
@@ -6,13 +6,16 @@ wb = Workbook()
 ws = wb.active
 with open('Task_11.2_csv.csv', 'r', encoding='utf-8') as file:
     rows = csv.reader(file)
-    sor_rows = sorted(rows, key = lambda x:(x[3], x[1], x[2]))
+    headers = next(rows) # По поводу nextа - вычитал в интернете, но почему он изъял заголовки из rows - я не понял)
+    # print(headers)
+    sor_rows = sorted((rows), key=lambda x: (x[3], x[1], x[2]))
+    # print(sor_rows)
+    ws.append(headers)
     for row in sor_rows:
         ws.append(row)
     summ = 0
     for i in range(len(sor_rows)):
-        for j in range(5):
-            summ += int(sor_rows[i][4])
-print(summ)
-ws.append(['Итого',summ])
+       summ += int(sor_rows[i][4])
+# print(summ)
+ws.append(['','','','Итого',summ])
 wb.save('Task_11.2_xl.xlsx')
